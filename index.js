a0092b0ee71b020d7dbdf0576a5de24e739a86b9
margaux <emjeon93@gmail.com>
2019-07-31 18:11:28 +0900

    schema 와 resolver 파일 분리

--- index.js
+++ index.js
@@ -1,19 +1,11 @@
 const express = require('express');
-const { ApolloServer, gql } = require('apollo-server-express');
+const { ApolloServer } = require('apollo-server-express');
 
 // Construct a schema, using GraphQL schema language
-const typeDefs = gql`
-  type Query {
-    hello: String
-  }
-`;
+const typeDefs = require('./schema');
 
 // Provide resolver functions for your schema fields
-const resolvers = {
-  Query: {
-    hello: () => 'Hello world!',
-  },
-};
+const resolvers = require('./resolvers');
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
